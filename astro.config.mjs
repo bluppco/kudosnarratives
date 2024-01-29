906ac17f51025de2d7c45b8939aa53296bb99a4c
Mohit <mohit@hey.com>
2024-01-29 15:45:11 +0530

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    runtime: {
+      mode: 'local',
+      type: 'pages'
+    }
+  }),
+  integrations: [tailwind({applyBaseStyles: false}), react(), mdx()]
+});
